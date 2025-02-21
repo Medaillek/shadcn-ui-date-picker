546984dfd05c4b23ce4b0d3e5e201f7b6b5e2944
Medaillek <medaillek@gmail.com>
2025-02-21 18:13:01 +0100

    REFactor Calendar component to enhance functionality and styling

--- calendar.tsx
+++ calendar.tsx
@@ -1,75 +1,196 @@
-import * as React from 'react'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
-import { DayPicker } from 'react-day-picker'
+'use client'
 
-import { fr } from 'date-fns/locale'
+import 'react-day-picker/style.css'
+import { fr } from 'react-day-picker/locale'
 
-import { cn } from '@/src/utils'
-import { buttonVariants } from '@/components/ui/button'
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
+import {
+	DayPicker,
+	labelNext,
+	labelPrevious,
+	useDayPicker,
+} from 'react-day-picker'
+import { cn } from '@/lib/utils'
+import { Button, buttonVariants } from '@/components/ui/button'
+import {
+	Select,
+	SelectTrigger,
+	SelectContent,
+	SelectItem,
+	SelectValue,
+	SelectGroup,
+} from '@/components/ui/select'
+import { frenchMonthsDropDownOptions } from '@/data/dates'
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+function Calendar({
+	className,
+	classNames,
+	showOutsideDays = true,
+	...props
+}: React.ComponentProps<typeof DayPicker>) {
+	return (
+		<DayPicker
+			timeZone="Europe/Paris"
+			showOutsideDays={showOutsideDays}
+			weekStartsOn={1}
+			locale={fr}
+			showWeekNumber
+			className={cn('py-2', className)}
+			classNames={{
+				months: 'relative flex flex-col gap-y-4 sm:flex-row sm:gap-y-0',
+				month_caption:
+					'relative mx-10 flex h-7 items-center justify-center pt-2',
+				weekdays:
+					'flex flex-row w-full items-center justify-center gap-x-1 ml-3',
+				weekday: 'w-8 text-[0.8rem] font-normal text-muted-foreground',
+				month: 'w-full gap-y-4 overflow-x-hidden',
+				caption: 'relative flex items-center justify-center pt-1',
+				caption_label: 'truncate text-sm font-medium',
+				button_next: cn(
+					buttonVariants({
+						variant: 'outline',
+						className:
+							'absolute right-0 h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+					})
+				),
+				button_previous: cn(
+					buttonVariants({
+						variant: 'outline',
+						className:
+							'absolute left-0 h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
+					})
+				),
+				nav: 'flex items-start pt-1',
+				month_grid: 'mb-2 mx-2 mt-3',
+				week: 'mt-2 flex w-full',
+				day: 'flex h-9 w-9 flex-1 items-center justify-center rounded-md p-0 text-sm [&:has(button)]:hover:bg-accent! [&:has(button)]:hover:text-accent-foreground [&:has(button)]:hover:aria-selected:bg-primary! [&:has(button)]:hover:aria-selected:text-primary-foreground',
+				day_button: cn(
+					buttonVariants({ variant: 'ghost' }),
+					'h-9 w-9 p-0 font-normal transition-none hover:bg-transparent hover:text-inherit aria-selected:opacity-100'
+				),
+				range_start: 'day-range-start rounded-s-md',
+				range_end: 'day-range-end rounded-e-md',
+				selected:
+					'bg-primary text-primary-foreground hover:bg-primary! hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+				today: 'bg-accent text-accent-foreground',
+				outside:
+					'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
+				disabled: 'text-muted-foreground opacity-40',
+				range_middle:
+					'rounded-none aria-selected:bg-accent aria-selected:text-accent-foreground hover:aria-selected:bg-accent! hover:aria-selected:text-accent-foreground',
+				hidden: 'invisible hidden',
+				chevron: `inline-block fill-muted-foreground`,
+				week_number:
+					'text-muted-foreground font-normal bg-muted text-xs flex items-end justify-center h-9 w-7 py-2',
+				week_number_header: 'text-muted-foreground',
+				...classNames,
+			}}
+			components={{
+				Dropdown: ({ ...props }) => {
+					const { options, className, disabled } = props
+					const { goToMonth, months } = useDayPicker()
+					const currentShown = months[0].date
 
-function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
-  const isDropDown = props.captionLayout?.includes('dropdown')
-  return (
-    <DayPicker
-      showOutsideDays={showOutsideDays}
-      className={cn('p-3', className)}
-      locale={fr}
-      lang="fr"
-      classNames={{
-        months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
-        month: 'space-y-4',
-        caption: `flex justify-center pt-1 relative items-center ${
-          isDropDown ? 'w-full flex-col gap-2' : ''
-        }`,
-        caption_label: `text-sm font-medium ${isDropDown ? 'hidden' : ''}`,
-        nav: `space-x-1 flex items-center ${isDropDown ? 'justify-between w-full' : ''}`,
-        nav_button: cn(
-          buttonVariants({ variant: 'outline' }),
-          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100',
-        ),
+					const currentSelection =
+						className === 'rdp-years_dropdown'
+							? currentShown.getFullYear().toString()
+							: currentShown.getMonth().toString()
 
-        caption_dropdowns: 'flex w-full gap-2',
-        dropdown_month: 'relative flex-1',
-        vhidden: `${isDropDown ? 'hidden' : ''}`,
+					const updateDayPickerState = (value: string) => {
+						const newDate = new Date(currentShown)
+						if (className === 'rdp-years_dropdown') {
+							newDate.setFullYear(parseInt(value))
+						} else if (className === 'rdp-months_dropdown') {
+							newDate.setMonth(parseInt(value))
+						}
+						goToMonth(newDate)
+					}
 
-        dropdown_year: 'relative min-w-[5rem]',
-        dropdown_icon: '',
-        dropdown: cn(
-          'w-full p-2 bg-white shadow-md rounded-md z-10',
-          'space-y-2 border border-gray-200 text-sm',
-        ),
+					return (
+						<Select
+							value={currentSelection}
+							onValueChange={updateDayPickerState}
+							disabled={disabled}
+						>
+							<SelectTrigger className="flex items-center w-full gap-1 px-2 border-0 hover:bg-accent hover:text-accent-foreground h-7">
+								<SelectValue />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									{options?.map((option) => (
+										<SelectItem
+											key={option.value}
+											value={option.value.toString()}
+										>
+											{option.label}
+										</SelectItem>
+									))}
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+					)
+				},
+				MonthsDropdown: ({ ...props }) => {
+					const { components } = useDayPicker()
+					const options = frenchMonthsDropDownOptions
 
-        nav_button_previous: `${isDropDown ? 'block' : 'absolute left-1'}`,
-        nav_button_next: `${isDropDown ? 'block' : 'absolute right-1'}`,
-        table: 'w-full border-collapse space-y-1',
-        head_row: 'flex',
-        head_cell: 'text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]',
-        row: 'flex w-full mt-2',
-        cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-        day: cn(
-          buttonVariants({ variant: 'ghost' }),
-          'h-9 w-9 p-0 font-normal aria-selected:opacity-100',
-        ),
-        day_range_end: 'day-range-end',
-        day_selected:
-          'bg-primary text-white hover:bg-primary hover:text-white focus:bg-primary focus:text-white',
-        day_today: 'bg-accent text-accent-foreground',
-        day_outside:
-          'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
-        day_disabled: 'text-muted-foreground opacity-50',
-        day_range_middle: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
-        day_hidden: 'invisible',
-        ...classNames,
-      }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
-      {...props}
-    />
-  )
+					return (
+						<components.Dropdown
+							{...props}
+							options={options}
+						/>
+					)
+				},
+
+				YearsDropdown: ({ ...props }) => {
+					const { components } = useDayPicker()
+					// sort years in descending order
+					const options = props.options?.sort((a, b) => b.value - a.value) ?? []
+
+					return (
+						<components.Dropdown
+							{...props}
+							options={options}
+						/>
+					)
+				},
+				PreviousMonthButton: ({ className, ...props }) => {
+					const previousMonth = useDayPicker().previousMonth
+					return (
+						<Button
+							variant="ghost"
+							className={cn('absolute left-0 ml-2 disabled:hidden', className)}
+							type="button"
+							tabIndex={previousMonth ? -1 : undefined}
+							disabled={!previousMonth}
+							aria-label={labelPrevious(previousMonth)}
+							onClick={props.onClick}
+						>
+							<ChevronLeftIcon className="w-4 h-4" />
+						</Button>
+					)
+				},
+				NextMonthButton: ({ className, ...props }) => {
+					let nextMonth = useDayPicker().nextMonth
+
+					return (
+						<Button
+							variant="ghost"
+							className={cn('absolute right-0 mr-2 disabled:hidden', className)}
+							type="button"
+							tabIndex={nextMonth ? -1 : undefined}
+							disabled={!nextMonth}
+							aria-label={labelNext(nextMonth)}
+							onClick={props.onClick}
+						>
+							<ChevronRightIcon className="w-4 h-4" />
+						</Button>
+					)
+				},
+			}}
+			{...props}
+		/>
+	)
 }
 Calendar.displayName = 'Calendar'
 
